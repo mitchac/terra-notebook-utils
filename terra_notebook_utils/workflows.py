59f13275121028173286c316e6992b5908123352
mitchac <mitchac@gmail.com>
2021-05-02 13:31:54 +1000

    Update workflows.py

--- terra_notebook_utils/workflows.py
+++ terra_notebook_utils/workflows.py
@@ -90,7 +90,7 @@ def estimate_workflow_cost(workflow_id: str, workflow_metadata: dict) -> Generat
                     completed = js_get("executionStatus", call_metadata)
                     print(completed)
                     if completed == "Done":
-                        instance_start = datetime.strptime(js_get("executionEvents[?description == 'waiting for quota'].endTime | [0]", call_metadata), date_format)
+                        instance_start = datetime.strptime(js_get("executionEvents[?contains(description,'assigned')].startTime | [0]", call_metadata), date_format)
                         instance_end = datetime.strptime(js_get("executionEvents[?description == 'Worker released'].endTime | [0]", call_metadata), date_format)
                         runtime = (instance_end - instance_start).total_seconds()
                     else:
